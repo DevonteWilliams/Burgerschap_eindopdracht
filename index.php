cf3331e28539246c71da566ec6c5c253eacabdcc
Ta1Vos <302196386@student.rocmondriaan.nl>
2023-10-05 09:59:17 +0200

    Added basic template

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Eindopdracht BUR p05</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
@@ -16,17 +16,26 @@
     </div>
     <div class="row mt-3">
         <div class="col-md">
-            h1.
+            <div class="fs1">Hoe maak je een CV?</div>
+            Lorem ipsum des lorem ipsumus
         </div>
     </div>
     <div class="row">
         <div class="col-md">
-
+            <div class="fs1">Tips voor motivatiebrieven</div>
+            Hehehhehehehehehehehe this text is to display my point >:3
+            <ul>
+                <li>Integrity</li>
+                <li>Personality</li>
+                <li>Showing your value</li>
+                <li>Why they should hire you</li>
+            </ul>
         </div>
     </div>
     <div class="row">
         <div class="col-md">
-
+            <div class="fs1">Hoe jij je aanwezigheid laat merken</div>
+            Text to display my point :D
         </div>
     </div>
     <div class="row">
