36b3db659ee38ed1b1b3839f4cef3c09757b56ac
D.Williams <302477878@student.rocmondriaan.nl>
2023-10-10 14:21:29 +0200

    I dunno anymore

--- index.php
+++ index.php
@@ -6,16 +6,17 @@
     <title>Eindopdracht BUR p05</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 </head>
 <body>
 <div class="container-fluid text-center">
     <div class="row">
-        <div class="col-md">
-            <h1 class="text-center text-dark-emphasis">Test</h1>
+        <div class="col-md mt-3">
+            <h1 class="text-center text-dark-emphasis">Tips & Tricks voor het solliciteren</h1>
         </div>
     </div>
     <div class="row mt-3">
-        <div class="col-md">
+        <div class="col-md mt-2">
             <div class="fs-1">Hoe maak je een CV?</div>
             Lorem ipsum des lorem ipsumus
         </div>
@@ -25,7 +26,7 @@
             <div class="fs-1">Tips voor motivatiebrieven</div>
             Hehehhehehehehehehehe this text is to display my point >:3
             <div>
-                Sym<span>Integrity</span><br>
+                Sym<span> Integrity</span><br>
                 Sym<span>Personality</span><br>
                 Sym<span>Showing your value</span><br>
                 Sym<span>Why they should hire you</span><br>
